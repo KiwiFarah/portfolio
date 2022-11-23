6583dab17202d897a18519ab9298ef8fac077366
KiwiFarah <warnakulasuriyafarah@gmail.com>
2022-11-23 13:37:59 +0400

    add contacts component and smooth scrolling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a73f7b94d79252ae"; }
+/******/ 		__webpack_require__.h = function() { return "d3f80ae198a4cbff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
@@ -9,13 +9,31 @@ import Logo from '../public/assets/NavbarLogo.png'
 
 
 const Navbar = () => {
-  const [nav,setNav] = useState(false)
+  const [nav,setNav] = useState(false);
+  const [shadow,setShadow] = useState(false);
 
   const handleNav = () =>{
   setNav(!nav)
   }
+
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener('scroll', handleShadow);
+  }, []);
+
   return (
-    <div className="fixed w-full h-20 shadow-xl z-[100]">
+    <div 
+    className={
+      shadow
+        ? 'fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300'
+        : 'fixed w-full h-20 z-[100]'
+    } >
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Image
           //src="/../public/assets/NavbarLogo.png"
@@ -29,18 +47,18 @@ const Navbar = () => {
             <Link href="/">
               <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
             </Link>
-            <Link href="/">
+            <Link href="/#about">
               <li className="ml-10 text-sm uppercase hover:border-b">About</li>
             </Link>
-            <Link href="/">
+            <Link href="/#skills">
               <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
             </Link>
-            <Link href="/">
+            <Link href="/#projects">
               <li className="ml-10 text-sm uppercase hover:border-b">
                 Projects
               </li>
             </Link>
-            <Link href="/">
+            <Link href="/#contact">
               <li className="ml-10 text-sm uppercase hover:border-b">
                 Contact
               </li>
