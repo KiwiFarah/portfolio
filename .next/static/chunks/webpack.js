cd0bd73791772b4e686af3df02249805ea275f26
KiwiFarah <warnakulasuriyafarah@gmail.com>
2022-11-23 15:15:40 +0400

    router projects section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e44ab21770257890"; }
+/******/ 		__webpack_require__.h = function() { return "89f6b8225cb0caa3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AboutImg from "../public/assets/about.jpg";
 import Image from "next/image";
+import Link from "next/link";
 
 function About() {
   return (
@@ -30,7 +31,7 @@ function About() {
           </p>
           
           <p className="py-2 text-gray-600 underline cursor-pointer">
-            Check out some of my latest projects.
+          <Link href="/#projects">Check out some of my latest projects.</Link>
           </p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
